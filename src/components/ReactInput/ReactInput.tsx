8019819101823336c5d74a594186764dad4bcb5f
elkick <elkick.official@gmail.com>
2022-04-10 23:00:27 +0530

    initialial commit and setup project

---
+++ src/components/ReactInput/ReactInput.tsx
@@ -0,0 +1,59 @@
+import { FunctionComponent } from 'react'
+import { Form } from 'react-bootstrap'
+import { ReactInputProps } from './ReactInput.types'
+
+export const ReactInput: FunctionComponent<ReactInputProps> = ({
+  id,
+  inputType,
+  inputName,
+  inputValue,
+  inputPlaceholder,
+  dataTestId,
+  isReadOnly,
+  inputClassName,
+  inputAutoComplete,
+  inputLabelText,
+  inputGroupClassName,
+  isMultiple,
+  inputErrorText,
+  inputAcceptType,
+  inputHandleChange,
+  inputHandleBlur,
+  inputHandleOnKeyDown,
+  inputHandleOnKeyUp,
+}) => {
+  const onBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    if (inputHandleBlur) inputHandleBlur(event)
+  }
+
+  return (
+    <>
+      <Form.Group controlId={id} className={inputGroupClassName}>
+        <div className="position-relative">
+          <Form.Label className="input-label">{inputLabelText}</Form.Label>
+          <Form.Control
+            data-test-id={dataTestId}
+            type={inputType}
+            placeholder={inputPlaceholder}
+            name={inputName}
+            readOnly={isReadOnly}
+            value={inputValue}
+            className={inputClassName}
+            autoComplete={inputAutoComplete}
+            multiple={isMultiple}
+            accept={inputAcceptType}
+            onBlur={onBlur}
+            onChange={inputHandleChange}
+            onKeyDown={inputHandleOnKeyDown}
+            onKeyUp={inputHandleOnKeyUp}
+          />
+          {inputErrorText && inputErrorText !== '' && (
+            <>
+              <span className="warning-msg-text">{inputErrorText}</span>
+            </>
+          )}
+        </div>
+      </Form.Group>
+    </>
+  )
+}
---
+++ src/components/ReactInput/ReactInput.types.ts
@@ -0,0 +1,30 @@
+import React, { HTMLInputTypeAttribute } from 'react'
+export type ReactInputProps = {
+  id: string
+  inputType:
+    | 'text'
+    | 'date'
+    | 'email'
+    | 'search'
+    | 'number'
+    | 'file'
+    | 'password'
+    | 'hidden'
+    | 'time'
+  inputName: string
+  inputValue: string
+  inputPlaceholder?: string
+  dataTestId?: string
+  isReadOnly?: boolean
+  inputClassName?: string
+  inputAutoComplete?: 'on' | 'off'
+  inputLabelText?: string
+  inputGroupClassName?: string
+  isMultiple?: boolean
+  inputErrorText?: string
+  inputAcceptType?: string
+  inputHandleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  inputHandleBlur?: (event: React.ChangeEvent<HTMLInputElement>) => void
+  inputHandleOnKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void
+  inputHandleOnKeyUp?: (event: React.KeyboardEvent<HTMLInputElement>) => void
+}
